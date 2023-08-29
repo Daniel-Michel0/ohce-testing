0312ceeac712e5d97e2f4a53803376b7aba4e97b
Daniel Michel <timydan@gmail.com>
2023-08-29 17:21:36 -0400

    Added implementation for reverse word test
    Separated main function into smaller functions

--- ohce.py
+++ ohce.py
@@ -4,11 +4,31 @@ import datetime
 def ohce(nombre):
     hora_actual = int(datetime.datetime.now().hour)
     if 12 <= hora_actual and hora_actual < 20:
-        return "¡Buenas tardes " + nombre + "!"
+        return ("¡Buenas tardes " + nombre + "!")
     elif 6 <= hora_actual < 12:
-        return "¡Buenos días " + nombre + "!"
+        return ("¡Buenos días " + nombre + "!")
     else:
-        return "¡Buenas noches " + nombre + "!"
+        print("¡Buenas noches " + nombre + "!")    
+    stop = False
+    while not stop:
+        voltear = input()
+        print(voltear[::-1])
+
+def saludos(nombre):
+    hora_actual = int(datetime.datetime.now().hour)
+    if 12 <= hora_actual and hora_actual < 20:
+        return ("¡Buenas tardes " + nombre + "!")
+    elif 6 <= hora_actual < 12:
+        return ("¡Buenos días " + nombre + "!")
+    else:
+        return ("¡Buenas noches " + nombre + "!")   
+
+def voltear(palabra):
+        voltear = input()
+        print(voltear[::-1])
+
+def main():
+    ohce()
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
--- test_ohce.py
+++ test_ohce.py
@@ -7,26 +7,26 @@ def test_noche():
     nombre = "Juan"
     with patch("ohce.datetime") as mock_datetime:
         mock_datetime.datetime.now.return_value.hour = 3
-        assert ohce.ohce(nombre) == "¡Buenas noches " + nombre + "!"
+        assert ohce.saludos(nombre) == "¡Buenas noches " + nombre + "!"
 
 def test_mañana():
     nombre = "Carlos"
     with patch("ohce.datetime") as mock_datetime:
         mock_datetime.datetime.now.return_value.hour = 9
-        assert ohce.ohce(nombre) == "¡Buenos días " + nombre + "!"
+        assert ohce.saludos(nombre) == "¡Buenos días " + nombre + "!"
 
 def test_tarde():
     nombre = "Robert"
     with patch("ohce.datetime") as mock_datetime:
         mock_datetime.datetime.now.return_value.hour = 15
-        assert ohce.ohce(nombre) == "¡Buenas tardes " + nombre + "!"
+        assert ohce.saludos(nombre) == "¡Buenas tardes " + nombre + "!"
 
 def test_voltear():
     input_values = ["palindromo"]
-    expected_output = ["omordnilap"]
+    expected_output = "omordnilap\n"
 
     with patch("builtins.input", side_effect=input_values), patch("sys.stdout", new_callable=io.StringIO) as mock_stdout:
-        ohce.ohce("Emily")
+        ohce.voltear(input_values[0])
 
         output = mock_stdout.getvalue()
-        assert output == expected_output[0]
+        assert output == expected_output
