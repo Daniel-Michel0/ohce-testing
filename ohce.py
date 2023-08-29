369451e475bed192a9ecf035bf8df006d60795d4
Daniel Michel <timydan@gmail.com>
2023-08-29 01:25:37 -0400

    (not so) minimal implementation to pass 3 tests
    small fixes

--- ohce.py
+++ ohce.py
@@ -2,7 +2,13 @@ import sys
 import datetime
 
 def ohce(nombre):
-    return "¡Buenas noches " + nombre + "!"
+    hora_actual = int(datetime.datetime.now().hour)
+    if 12 <= hora_actual and hora_actual < 20:
+        return "¡Buenas tardes " + nombre + "!"
+    elif 6 <= hora_actual < 12:
+        return "¡Buenos días " + nombre + "!"
+    else:
+        return "¡Buenas noches " + nombre + "!"
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
--- test_ohce.py
+++ test_ohce.py
@@ -1,22 +1,21 @@
 import ohce
 from unittest.mock import patch
 import datetime
-## sys.argv[0]
 
 def test_noche():
     nombre = "Juan"
     with patch("ohce.datetime") as mock_datetime:
-        mock_datetime.datatime.now.return_value.hour = 3
+        mock_datetime.datetime.now.return_value.hour = 3
         assert ohce.ohce(nombre) == "¡Buenas noches " + nombre + "!"
 
 def test_mañana():
-    nombre = "Juan"
+    nombre = "Carlos"
     with patch("ohce.datetime") as mock_datetime:
-        mock_datetime.datatime.now.return_value.hour = 9
+        mock_datetime.datetime.now.return_value.hour = 9
         assert ohce.ohce(nombre) == "¡Buenos días " + nombre + "!"
 
 def test_tarde():
-    nombre = "Juan"
+    nombre = "Robert"
     with patch("ohce.datetime") as mock_datetime:
-        mock_datetime.datatime.now.return_value.hour = 15
+        mock_datetime.datetime.now.return_value.hour = 15
         assert ohce.ohce(nombre) == "¡Buenas tardes " + nombre + "!"
