232330bbc8d84526c09a4fe884adacf0acab2414
Daniel Michel <timydan@gmail.com>
2023-09-01 13:58:02 -0400

    Small fixes, built the main function

--- ohce.py
+++ ohce.py
@@ -2,17 +2,14 @@ import sys
 import datetime
 
 def ohce(nombre):
-    hora_actual = int(datetime.datetime.now().hour)
-    if 12 <= hora_actual and hora_actual < 20:
-        return ("¡Buenas tardes " + nombre + "!")
-    elif 6 <= hora_actual < 12:
-        return ("¡Buenos días " + nombre + "!")
-    else:
-        print("¡Buenas noches " + nombre + "!")    
-    stop = False
-    while not stop:
-        voltear = input()
-        print(voltear[::-1])
+    print(saludos(nombre))
+    while True:
+        palabra = input()
+        if adios([palabra, nombre]):
+            return 0;
+        print(palabra[::-1])
+        palindromo(palabra)
+
 
 def saludos(nombre):
     hora_actual = int(datetime.datetime.now().hour)
@@ -35,7 +32,14 @@ def palindromo(palabra):
 def adios(palabras):
     if palabras[0] == "stop!":
         print("Adios " + palabras[1])
+        return True
+    return False
+
+def main(nombre):
+    nombre = sys.argv[1]
+    ohce(nombre)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         nombre = sys.argv[1]
+        main(nombre)
